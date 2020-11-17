b74132293fef44d93eb10cebfaae535019668953
Yasin <adan.a.yasin@outlook.com>
2020-11-17 10:55:19 -0600

    added chart.

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ export default class App extends Component {
       <div className="App">
         <div className={styles.container}>
           <Cards data={data}/>
-          <Chart />
           <CountryPicker />
+          <Chart />
         </div>
       </div>
     );
